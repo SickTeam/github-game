e32e3524f275293b9489bd57ce1c98dd133638d9
MikaelEC <mikaelec@gmail.com>
2015-03-09 21:34:50 +0100

    Added contributors list with checkboxes

--- src/js/start-controller.js
+++ src/js/start-controller.js
@@ -5,18 +5,12 @@
       $scope.pickRepo = function (item, model, label) {
         Contributors.query({ owner: $scope.ownerName, repo: item },
           function (r, headers) {
-            var totalCommits = 0;
-            $scope.contributors = r.length;
-
-            r.forEach(function (x) {
-              totalCommits += x.contributions;
+            $scope.contributors = r.map(function (x) {
+              return { login: x.login, contributions: x.contributions, selected: true };
             });
-            $scope.commits = totalCommits;
-            $scope.checked = true;
           },
           function (headers) {
             $rootScope.addAlert('Owner and/or repo not found', 'danger');
-            $scope.checked = false;
           });
       };
     }]);
