52bf0230b2dd761522605c3a978e4ab2915f9d6d
MikaelEC <mikaelec@gmail.com>
2015-03-10 12:27:19 +0100

    Changed contributors to object to handle guess state

--- src/js/game-controller.js
+++ src/js/game-controller.js
@@ -3,6 +3,9 @@
 
     .controller('GameController', ['$scope', '$rootScope', '$routeParams', 'Commits', function ($scope, $rootScope, $routeParams, Commits) {
       $scope.contributors = $routeParams.contributors.split(',');
+      $scope.contributors = $scope.contributors.map(function (x) {
+        return { login: x, trueNeg: false, truePos: false, falsePos: false };
+      });
       console.log($scope.contributors);
       Commits.query( { owner: $routeParams.owner, repo: $routeParams.repo }, function (data, headers) {
         $scope.commits = data.map(function (x) {
@@ -15,10 +18,11 @@
         Commits.get( { owner: $routeParams.owner, repo: $routeParams.repo, sha: sha}, function (data, headers) {
           $scope.commit = data;
         });
+        $scope.committer = $scope.contributors[0];
       });
 
       $scope.makeGuess = function (con) {
-
+        
       };
     }]);
 
