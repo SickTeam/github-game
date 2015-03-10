1ce0b3ebbf7ffe9ff6973712bcc013757f43dd61
MikaelEC <mikaelec@gmail.com>
2015-03-10 12:16:05 +0100

    Added retrieval of single commit and update of interface on retrieved

--- src/js/game-controller.js
+++ src/js/game-controller.js
@@ -2,11 +2,24 @@
   angular.module('githubgame')
 
     .controller('GameController', ['$scope', '$rootScope', '$routeParams', 'Commits', function ($scope, $rootScope, $routeParams, Commits) {
+      $scope.contributors = $routeParams.contributors.split(',');
+      console.log($scope.contributors);
       Commits.query( { owner: $routeParams.owner, repo: $routeParams.repo }, function (data, headers) {
         $scope.commits = data.map(function (x) {
           return x.sha;
         });
+        $scope.$broadcast('commit-get', $scope.commits.pop());
       });
+
+      $scope.$on('commit-get', function (event, sha) {
+        Commits.get( { owner: $routeParams.owner, repo: $routeParams.repo, sha: sha}, function (data, headers) {
+          $scope.commit = data;
+        });
+      });
+
+      $scope.makeGuess = function (con) {
+
+      };
     }]);
 
 })();
