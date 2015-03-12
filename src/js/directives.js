78a1d6ce9670a3bc23ee92044fffb4dd9b464c3d
MikaelEC <mikaelec@gmail.com>
2015-03-12 22:20:42 +0100

    Added directive for handling min contributors selected, solves #6

--- src/js/directives.js
+++ src/js/directives.js
@@ -81,6 +81,28 @@
           });
         }
       };
+    }])
+
+    .directive('contributorsSelected', [function () {
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+          return scope.$watch('contributors', function (contribs) {
+            if (!contribs)
+              return ngModel.$setValidity('enoughChecked', false);
+            else
+              return ngModel.$setValidity('enoughChecked',
+                (function (x) {
+                  var noChecked = 0;
+                  for (var i = 0; i < x.length; i++)
+                    if (x[i].selected)
+                      noChecked++;
+                  return noChecked > 1;
+                })(contribs));
+          }, true);
+        }
+      };
     }]);
 
 })();
