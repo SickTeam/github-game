5ff158b9e4795c629ef5c7820489c9fde31ccd60
MikaelEC <mikaelec@gmail.com>
2015-03-15 11:28:46 +0100

    Made recursive get of commits until all are retrieved, resolves #9

--- src/js/game-controller.js
+++ src/js/game-controller.js
@@ -6,6 +6,7 @@
       $scope.contributors = $scope.contributors.map(function (x) {
         return { login: x, trueNeg: false, truePos: false, falsePos: false };
       });
+      $scope.commits = [];
       $scope.stats = { correct: 0, incorrect: 0, total: 0, remaining: 0};
 
       $scope.$on('commit-get', function (event, sha) {
@@ -47,29 +48,40 @@
         $scope.$broadcast('commit-get', $scope.commits.pop());
       };
 
-      Commits.query( { owner: $routeParams.owner, repo: $routeParams.repo }, function (data, headers) {
-        var d = data;
-        var isContributor = function (x) {
-          for (var i = 0; i < $scope.contributors.length; i++) {
-            if (x == $scope.contributors[i].login)
-              return true;
-          }
-          return false;
-        };
-        d = d.filter(function (x) {
-          return isContributor(x.author.login);
-        });
-        d = d.map(function (x) {
-          return x.sha;
-        });
-        d = (function (arr) {
-          for (var j, x, i = arr.length; i; j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-          return arr;
-        })(d);
-        $scope.stats.remaining = d.length;
-        $scope.commits = d;
-        $scope.$broadcast('commit-get', $scope.commits.pop());
-      });
+      var getCommits = function (page) {
+        Commits.query( { owner: $routeParams.owner, repo: $routeParams.repo, page: page },
+          function (data, headers) {
+            if (data !== undefined && data.length > 0) {
+              var d = data;
+              var isContributor = function (x) {
+                for (var i = 0; i < $scope.contributors.length; i++) {
+                  if (x == $scope.contributors[i].login)
+                    return true;
+                }
+                return false;
+              };
+              d = d.filter(function (x) {
+                return isContributor(x.author.login);
+              });
+              d = d.map(function (x) {
+                return x.sha;
+              });
+              $scope.commits = $scope.commits.concat(d);
+
+              getCommits(page+1);
+            }
+            else {
+              $scope.commits = (function (arr) {
+                for (var j, x, i = arr.length; i; j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+                return arr;
+              })($scope.commits);
+              $scope.stats.remaining = $scope.commits.length;
+              $scope.$broadcast('commit-get', $scope.commits.pop());
+            }
+          });
+      };
+
+      getCommits(1);
 
     }]);
 
