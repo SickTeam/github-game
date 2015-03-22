6db840204b70482013e561e1352ee15f15e1229e
MikaelEC <mikaelec@gmail.com>
2015-03-22 19:29:52 +0100

    Removed ftp-deploy from dist task, as it is not currently working

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,6 +191,6 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('dev', ['clean:dev', 'uglify:dev', 'copy:dev', 'processhtml:dev', 'cssmin:dev']);
-  grunt.registerTask('dist', ['clean:dist', 'uglify:dist', 'copy:dist', 'processhtml:dist', 'htmlmin:dist', 'cssmin:dist', 'ftp-deploy:dist']);
+  grunt.registerTask('dist', ['clean:dist', 'uglify:dist', 'copy:dist', 'processhtml:dist', 'htmlmin:dist', 'cssmin:dist']);
   grunt.registerTask('check', ['jshint', 'csslint', 'htmlangular', 'clean:check']);
 };
