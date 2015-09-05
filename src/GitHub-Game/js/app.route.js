c692cd629d81d4349c278d96a3615220b025636e
mikaelec <mikaelec@gmail.com>
2015-09-05 14:42:40 +0200

    Refactored app.route to use vs angular file structure style

--- src/GitHub-Game/js/app.route.js
+++ src/GitHub-Game/js/app.route.js
@@ -1,22 +1,25 @@
 (function () {
-    angular.module('github-game')
+    angular
+        .module('github-game')
+        .config(config);
 
-      .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-          function ($stateProvider, $urlRouterProvider, $locationProvider) {
-              $locationProvider.html5Mode(true);
-              $urlRouterProvider.otherwise("/");
+    config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
-              $stateProvider
-                .state('start', {
-                    url: '/',
-                    templateUrl: 'html/start.layout.html',
-                    controller: 'startController as start'
-                })
-                .state('game', {
-                    url: '/game',
-                    templateUrl: 'html/game.layout.html',
-                    controller: 'gameController'
-                });
-          }]);
+    function config($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+          .state('start', {
+              url: '/',
+              templateUrl: 'html/start.layout.html',
+              controller: 'startController as start'
+          })
+          .state('game', {
+              url: '/game',
+              templateUrl: 'html/game.layout.html',
+              controller: 'gameController'
+          });
+    }
 
 })();
