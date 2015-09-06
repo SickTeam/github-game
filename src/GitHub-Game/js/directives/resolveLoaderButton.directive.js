f75a2f604f09feb33567f87fd53d7cd1f7a3b0ee
mikaelec <mikaelec@gmail.com>
2015-09-06 20:17:49 +0200

    Added spinner to start game button when starting game

---
+++ src/GitHub-Game/js/directives/resolveLoaderButton.directive.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('github-game')
+        .directive('resolveLoaderButton', resolveLoaderButton);
+
+    resolveLoaderButton.$inject = ['$rootScope', '$timeout'];
+
+    function resolveLoaderButton($rootScope, $timeout) {
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            var unregister = $rootScope.$on('$stateChangeStart', function () {
+                element.addClass('disabled');
+                var i = element.find('i');
+                i.removeClass('fa-arrow-right');
+                i.addClass('fa-spinner fa-spin');
+            });
+
+            scope.$on('$destroy', unregister);
+        }
+    }
+
+})();
