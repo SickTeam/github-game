592abe23eb78c1f156c7c057390a70d85ff17937
mikaelec <mikaelec@gmail.com>
2015-09-17 21:31:15 +0200

    Added message service with dummy getMessages

---
+++ src/GitHub-Game/js/services/message.service.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('github-game')
+        .factory('messageService', messageService);
+
+    messageService.$inject = ['$http', '$interval', '$q', '$timeout'];
+
+    function messageService($http, $interval, $q, $timeout) {
+        var CHECK_INTERVAL = 5000;
+        var timestamp;
+        var getMessages;
+
+        var service = {
+            start: start,
+            stop: stop
+        };
+
+        return service;
+
+        function start() {
+            $interval(getMessages, CHECK_INTERVAL);
+        }
+
+        function stop() {
+            $interval.cancel(getMessages);
+        }
+
+        function getMessages() {
+            return $q(function (resolve, reject) {
+                $timeout(() => resolve({ name: 'bogus', resource: { whatever: 'jah' } }), 350);
+            }).then(function (data) {
+                handleMessage(data.name, data.resource);
+            });
+        }
+
+        function handleMessage(name, resource) {
+            switch (name) {
+
+            }
+        }
+    }
+})();
