d03ae085c8948dc22296bef766ccba6a490e6d63
mikaelec <mikaelec@gmail.com>
2015-09-18 20:31:00 +0200

    Added game options to game.setup

--- src/GitHub-Game/js/controllers/game.setup.controller.js
+++ src/GitHub-Game/js/controllers/game.setup.controller.js
@@ -11,11 +11,28 @@
         var vm = this;
 
         vm.contributors = setup.contributors;
+        vm.options = extractOptions(setup);
 
         activate();
 
         function activate() {
 
+        }
+
+        function extractOptions(options) {
+            var extracted = [];
+
+            for (var key in options) {
+                if (key !== 'contributors') {
+                    var obj = {
+                        name: key,
+                        active: options[key]
+                    };
+                    extracted.push(obj);
+                }
+            }
+
+            return extracted;
         }
     }
 })();
