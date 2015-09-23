012bdaa79fca15277a668b1feef09060249f9eec
mikaelec <mikaelec@gmail.com>
2015-09-23 20:48:43 +0200

    Added seperate handling of resetting and getting round

--- src/GitHub-Game/js/controllers/game.started.controller.js
+++ src/GitHub-Game/js/controllers/game.started.controller.js
@@ -14,26 +14,39 @@
 
         function activate() {
             vm.params = params;
+            vm.players = players;
             vm.contributors = setup.contributors
                 .filter((obj) => obj.active)
                 .map((obj) => ({
-                    name: obj.name,
-                    guessed: false,
-                    correct: null
+                    name: obj.name
                 }));
 
-            apiService.getRound(vm.params.gameId, vm.params.round)
-                .then((response) => {
-                    vm.message = response.message;
-                    vm.linesAdd = response.linesAdd;
-                    vm.linesRemove = response.linesRemove;
-                }, (errorResponse) => {
-
-                });
+            _resetRound();
+            _getRound(vm.params.round);
 
             $rootScope.$on('roundstart', _handleRoundStart);
             $rootScope.$on('roundguess', _handleRoundGuess);
             $rootScope.$on('rounddone', _handleRoundDone);
+        }
+
+        function _getRound(round) {
+            apiService.getRound(vm.params.gameId, round)
+                .then((response) => {
+                    vm.message = response.message;
+                    vm.linesAdd = response.linesAdd;
+                    vm.linesRemove = response.linesRemove;
+
+                    return apiService.getGuesses(vm.params.gameId, round)
+                        .then((response) => {
+                            response.forEach((guesser) => {
+                                var player = vm.players[guesser.name];
+                                player.hasGuess = guesser.hasGuess;
+                                player.guess = guesser.guess;
+                            });
+                        });
+                }, (errorResponse) => {
+
+                });
         }
 
         function _handleRoundStart(resource) {
@@ -46,6 +59,21 @@
 
         function _handleRoundDone(resource) {
 
+        }
+
+        function _resetRound() {
+            vm.commit = '';
+            vm.linesAdd = 0;
+            vm.linesRemove = 0;
+
+            vm.contributors.forEach((contributor) => {
+                contributor.guessed = false;
+                contributor.correct = null;
+            });
+            for (var key in vm.players) {
+                vm.players[key].hasGuess = false;
+                vm.players[key].guess = null;
+            }
         }
     }
 })();
